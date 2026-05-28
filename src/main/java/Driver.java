b0b63438ce00928d498c85fbd58cf9eb449dcd67
raatra10 <rafi180350@gmail.com>
2026-05-28 17:29:56 +0700

    Refactor Driver class to improve output clarity
    Removed unused variables and consolidated print statements to use a constant message.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,26 +1,23 @@
+package src.main.java;
 import src.main.java.Counter;
 
 public class Driver {
 
+	private static final String CURRENT_COUNT_MSG = "Current count:";
+
 	public static void main(String[] args) {
-		
-		String unusedMessage = "This is a useless variable"; 
-		int temporaryNumber = 100;
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Current count:" + counter.getCount());
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println(CURRENT_COUNT_MSG + counter.getCount());
 		
 		counter.increment();
 		
-		System.out.println("Current count:" + counter.getCount());
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println(CURRENT_COUNT_MSG + counter.getCount());
 		
 		counter.decrement();
 		
-		System.out.println("Current count:" + counter.getCount());
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println(CURRENT_COUNT_MSG + counter.getCount());
 		
 	}
 
