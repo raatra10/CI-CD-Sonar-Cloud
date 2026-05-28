07adea116a58193f141b4f3292450321250a162f
raatra10 <rafi180350@gmail.com>
2026-05-28 18:40:50 +0700

    Refactor CounterTest and add new test cases
    Refactor assertions for clarity and add new tests for increase, decrease, multiplication, and power functionalities.

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -10,22 +10,21 @@ public class CounterTest {
 	public void testReset() {
 		Counter testCounter = new Counter();
 		
-		for(int i = 0; i <10; i++) {
+		for(int i = 0; i < 10; i++) {
 			testCounter.increment();
 		}
 		
 		testCounter.reset();
-		
-		assertEquals(testCounter.getCount(), 0);
+		assertEquals(0, testCounter.getCount());
 	}
 
 	@Test
 	public void testIncrement() {
 		Counter testCounter = new Counter();
 		
-		for(int i = 1; i <10; i++) {
+		for(int i = 1; i < 10; i++) {
 			testCounter.increment();
-			assertEquals(testCounter.getCount(), i);
+			assertEquals(i, testCounter.getCount());
 		}
 	}
 
@@ -33,10 +32,45 @@ public class CounterTest {
 	public void testDecrement() {
 		Counter testCounter = new Counter();
 		
-		for(int i = 1; i <10; i++) {
+		for(int i = 1; i < 10; i++) {
 			testCounter.decrement();
-			assertEquals(testCounter.getCount(), i * -1);
+			assertEquals(i * -1, testCounter.getCount());
 		}
 	}
 
+	@Test
+	public void testIncreaseAndDecreaseBy() {
+		Counter testCounter = new Counter();
+		
+		testCounter.increaseBy(5);
+		assertEquals(5, testCounter.getCount());
+		
+		testCounter.decreaseBy(3);
+		assertEquals(2, testCounter.getCount());
+	}
+
+	@Test
+	public void testMultiplicationAndTriple() {
+		Counter testCounter = new Counter();
+		
+		testCounter.increaseBy(2); 
+		testCounter.multiplyBy(4);  
+		assertEquals(8, testCounter.getCount());
+		
+		testCounter.triple();      
+		assertEquals(24, testCounter.getCount());
+	}
+
+	@Test
+	public void testPowerByAndEvenOddLogic() {
+		Counter testCounter = new Counter();
+		
+		assertTrue(testCounter.isCountEven());
+		
+		testCounter.increment(); 
+		assertFalse(testCounter.isCountEven());
+		
+		testCounter.powerBy(2);
+		assertEquals(3, testCounter.getCount());
+	}
 }
