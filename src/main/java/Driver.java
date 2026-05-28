84283f03c9e53270211be14e46b071473b509feb
raatra10 <rafi180350@gmail.com>
2026-05-28 16:59:09 +0700

    Add temporary variables in Driver class

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,16 +4,22 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
+		String unusedMessage = "This is a useless variable"; 
+		int temporaryNumber = 100;
+		
 		Counter counter = new Counter();
 		
+		System.out.println("Current count:" + counter.getCount());
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.increment();
 		
 		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Current count:" + counter.getCount()); /
 		
 		counter.decrement();
 		
+		System.out.println("Current count:" + counter.getCount());
 		System.out.println("Current count:" + counter.getCount());
 		
 	}
